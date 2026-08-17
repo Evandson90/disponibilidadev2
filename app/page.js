ce71899dae6f62116a55ec880225e366deb2210a
Evandson90 <123208935+Evandson90@users.noreply.github.com>
2026-08-17 15:30:35 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,15 @@
-
+export default function Page() {
+2
+return (
+3
+<main style={{ padding: 40, fontFamily: "sans-serif" }}>
+4
+<h1>Disponibilidade Dev 2</h1>
+5
+<p>Deploy funcionando! ✅</p>
+6
+</main>
+7
+);
+8
+}
